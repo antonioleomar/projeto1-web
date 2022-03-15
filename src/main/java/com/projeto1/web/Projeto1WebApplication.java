ee02fdf3bc26f7d0574198fcddfa22a63e306e45
antonio leomar <antonioleomarsoares@gmail.com>
2022-03-15 07:45:26 -0300

    projeto criado

---
+++ src/main/java/com/projeto1/web/Projeto1WebApplication.java
@@ -0,0 +1,13 @@
+package com.projeto1.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto1WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto1WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto1/web/Projeto1WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto1.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto1WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
