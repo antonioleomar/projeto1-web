dd7e2d817c9661be512c117beca7cbabd19c54fd
antonio leomar <antonioleomarsoares@gmail.com>
2022-03-15 21:18:21 -0300

    adicionado professor controller

--- src/main/java/com/projeto1/web/controller/ProfessorController.java
+++ src/main/java/com/projeto1/web/controller/ProfessorController.java
@@ -16,34 +16,43 @@ import org.springframework.web.bind.annotation.RestController;
 import com.projeto1.web.domain.Professor;
 import com.projeto1.web.repository.ProfessorRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/professor")
+@Api(value = "Professor Controller")
 public class ProfessorController {
 	
 	@Autowired
 	private ProfessorRepository professorRepository;
 	
 	@GetMapping
+	@ApiOperation(value = "Lista todos os professores")
 	public List<Professor> getProfessor(){
 		return professorRepository.findAll();
 	}
 	
 	@GetMapping("/{matricula}")
+	@ApiOperation(value = "Lista professor por ID")
 	public Optional<Professor> getProfessorById(@PathVariable Integer matricula){
 		return professorRepository.findById(matricula);
 	}
 	
 	@PostMapping
+	@ApiOperation(value = "criando um professor")
 	public Professor createProfessor(@RequestBody Professor professor) {
 		return professorRepository.save(professor);
 	}
 	
 	@PutMapping("/{matricula}")	
+	@ApiOperation(value = "Put de um professor")
 	public Professor updateProfessor(@PathVariable("matricula") Integer matricula, @RequestBody Professor professor) {
 		return professorRepository.save(professor);
 	}
 	
 	@DeleteMapping("/{matricula}")
+	@ApiOperation(value = "Deletar um professor por matricula")
 	public void deleteProfessor(@PathVariable Integer matricula) {
 		professorRepository.delete(professorRepository.findById(matricula).get());
 	}
